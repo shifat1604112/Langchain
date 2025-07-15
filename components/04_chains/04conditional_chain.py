6ae36607741cad339b781061228fe3210ffd8b69
Shifat Solaiman <solaimanshifat3@gmail.com>
2025-07-15 22:17:26 +0600

    added chain

---
+++ components/04_chains/04conditional_chain.py
@@ -0,0 +1,53 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableBranch, RunnableLambda
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro')
+
+parser = StrOutputParser()
+
+class Feedback(BaseModel):
+
+    sentiment: Literal['positive', 'negative'] = Field(description='Give the sentiment of the feedback')
+
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
+prompt1 = PromptTemplate(
+    template='Classify the sentiment of the following feedback text into postive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction':parser2.get_format_instructions()}
+)
+
+classifier_chain = prompt1 | model | parser2
+
+prompt2 = PromptTemplate(
+    template='Write an appropriate response to this positive feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template='Write an appropriate response to this negative feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+# RunnableBranch expects tuple in the form of (condition, chain) and lastly a default chain
+branch_chain = RunnableBranch(
+    (lambda x:x.sentiment == 'positive', prompt2 | model | parser),
+    (lambda x:x.sentiment == 'negative', prompt3 | model | parser),
+    RunnableLambda(lambda x: "could not find sentiment") # if pos/neg konota na hoy then eta execute hbe, but etato chain na, etak
+    # RunnableLambda er moddhe dile etaw chain er moto kaj krbe and output dbe like chain
+)
+
+chain = classifier_chain | branch_chain # chain.invoke er karone classifier_chain execute hoye positive/negative sentiment ashse,
+# then branch_chain a check dise if pos then execute first conditional chain elseif neg then second conditional chain else default something
+
+print(chain.invoke({'feedback': 'This is a beautiful phone'}))
+
+chain.get_graph().print_ascii()
